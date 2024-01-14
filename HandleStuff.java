42c273ff9c43aa962c86ca46ae68872ee2a54658
MehirNarayana <mehirkn@gmail.com>
2024-01-14 16:35:19 -0500

    added all my comments

--- HandleStuff.java
+++ HandleStuff.java
@@ -1,95 +1,125 @@
 import java.util.HashMap;
-
+/* 
+ *  The HandleStuff class extends the Graph class and 
+ * manages stores and their information
+ */
 class HandleStuff extends Graph{
-    String type;
-    double price;
-    double rating;
+
+
+    
+     
+    //represents all the stores in an array
     Store[] stores;
+    //represents the store that the user is currently in
     String currStop = "home";
     HashMap<String, Store> storeNames = new HashMap<>();
 
+
+    /* Constructor for HandleStuff class */
     public HandleStuff() {
         super();
-        //name, storerating, names, prices, clothes rating
+        //name, store rating, names, prices, clothes rating
+        // Calls the constructor of the superclass Graph
+        // Initialize stores with predefined values and add them to the storeNames hashmap
         Store store1 = new Store("High_Quality_Shirt", 4.2, new String[]{"black_shirt", "White_shirt", "Pink_shirt", "Green_shirt", "Orange_shirt"}, new double[]{30, 20, 30, 20, 15},new double[]{5, 5, 4, 4, 3});
         Store store2 = new Store("High_Quality_Pants", 4.1, new String[]{"short_pants","long_pants","black_jeans","blue_jeans","sweatpants"}, new double[]{30, 20, 30, 20, 15},new double[]{5, 4.5, 4, 4, 3});
         Store store3 = new Store("Pants_And_Shirts", 3.5, new String[]{"adidas_pants","puma_pants","nike_pants", "adidas_shirt", "puma_shirt", "nike_shirt"}, new double[]{25, 20, 20, 20, 17, 14},new double[]{4, 3.5, 2.5, 4, 3.5, 2.5});
         Store store4 = new Store("Shoe_Store", 3.8, new String[]{"running_shoes", "sneakers", "formal_shoes", "slippers", "sandals"}, new double[]{50, 40, 60, 25, 20}, new double[]{4.5, 4, 4.8, 3.5, 3});
         Store store5 = new Store("Underwear_Shirt_Store", 3.7, new String[]{"boxer_briefs", "briefs", "boxers", "v-neck_shirt", "crew_neck_shirt", "polo_shirt"}, new double[]{15, 18, 20, 25, 30, 35}, new double[]{4, 3.5, 3.8, 3.9, 4.2, 4.5});
         Store store6 = new Store("Everything_Store", 3.0, new String[]{"jeans", "chinos", "t-shirt", "polo_shirt", "sneakers", "loafers", "boxer_briefs", "briefs"}, new double[]{40, 30, 20, 25, 50, 60, 15, 18}, new double[]{4, 3.5, 3.8, 3.9, 4.5, 4.2, 4, 3.5});
+        //initialize the array with all the stores we just created
         stores = new Store[]{store1, store2, store3, store4, store5, store6};
-
+        //every store instance correponds to a store name
         storeNames.put("High_Quality_Shirt", store1);
         storeNames.put("High_Quality_Pants", store2);
         storeNames.put("Pants_And_Shirts", store3);
         storeNames.put("Shoe_Store", store4);
         storeNames.put("Underwear_Shirt_Store", store5);
         storeNames.put("Everything_Store", store6);
-        setHome();
-
-    }
-
-    public void updateStore(String name){
-        currStop = name;
-        setData(storeNames.get(currStop), stores);
-    }
-
-    public void setHome(){
-        setData(stores);
         
+
+    }           
+    /* Updates the current stop and 
+    sets the data for the graph based on the store name 
+    @param name - name of the store
+    @return void method returns nothing
+    */
+    public void updateStore(String name){
+        currStop = name; // Update the current stop to the given store name
+        setData(storeNames.get(currStop), stores); // Set the graph data for the current stop
+    }
+
+     
+    /* Resets the graph data to represent all stores as the current state 
+     * Called by the constructor. So it is called once every time the program is run
+     * @params none
+     * @return void method returns nothing
+    */
+    public void setHome(){
+        // resets the graph so that the distance and time attribute of each store is
+        //the distance from home
+        //this method is called when the instance of this class is created
+        setData(stores); 
+
     }
 
 
-    public double homeDistance(){
-        return distanceFromHome();
-    }
-
-    public double homeTime(){
-        return timeToHome();
-    }
+    
 
     
         
+    /* Retrieves a Store object based on the store name 
+     * @params storeName - the name of store whose instance needs to be accessed
+    */
 
     public Store getStore(String storeName){
+        //return the instance of the store using the hashmap
         return storeNames.get(storeName);
     }
 
 
     
-
+    /* Sorts the stores array by distance using bubble sort 
+    @param none
+    @return void method returns nothing
+    */
     public void sortByDistance(){
-        int storesNum = 7;
+        int storesNum = stores.length; // Use the length of the stores array for the number of stores
         for (int i = 0; i < storesNum - 1; i++){
             for (int j = 0; j < storesNum - i - 1; j++){
-                
-                
+                // Compare distances and swap if necessary
                 if (stores[j].getDistance() > stores[j + 1].getDistance()) {
-                        // Swap temp and arr[i]
-                        Store temp = stores[j];
-                        stores[j] = stores[j + 1];
+                        Store temp = stores[j]; // Temporary store for swapping
+                        stores[j] = stores[j + 1]; // Swap stores
                         stores[j + 1] = temp;
                 }
             }
         }
     }   
 
+    /* Sorts the stores array by time using bubble sort 
+    @param none
+    @return void method returns nothing
+    */
     public void sortByTime(){
-        int storesNum = 6;
+        int storesNum = stores.length; // Use the length of the stores array for the number of stores
         for (int i = 0; i < storesNum - 1; i++){
             for (int j = 0; j < storesNum - i - 1; j++){
-                
-                
+                // Compare times and swap if necessary
                 if (stores[j].getTime() > stores[j + 1].getTime()) {
-                        // Swap temp and arr[i]
-                        Store temp = stores[j];
-                        stores[j] = stores[j + 1];
+                        Store temp = stores[j]; // Temporary store for swapping
+                        stores[j] = stores[j + 1]; // Swap stores
                         stores[j + 1] = temp;
                 }
             }
         }
     }
     
+
+    /* Sorts the stores array by rating using bubble sort
+     * @param none
+     * @return void method returns nothing
+     */
     public void sortByRating(){
         int storesNum = 6;
         for (int i = 0; i < storesNum - 1; i++){
@@ -106,10 +136,20 @@ class HandleStuff extends Graph{
         }
     }
 
+    /* 
+     * Method to retrieve the array of Store objects
+     * @param nothing
+     * @return return the array of store sorted by distance, time or rating
+     */
     public Store[] getStores(){
+        
         return stores;
     }
 
+    /* Method to get the name of the current store
+     * @param none
+     * @return return the name of the current stop
+     */
     public String getCurrentStore(){
         return currStop;
     }
